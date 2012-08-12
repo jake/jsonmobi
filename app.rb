26f4206b708d185a619110cdeef7b9d71dd29413
Jacob Bijani <jacob.bijani@gmail.com>
2012-08-12 18:13:07 -0400

    full set of error messages

--- app.rb
+++ app.rb
@@ -10,10 +10,36 @@ error 400..505 do
   code = response.status
   
   message = {
-    500 => 'Error',
+    400 => 'Bad Request',
+    401 => 'Unauthorized',
+    402 => 'Payment Required',
     403 => 'Forbidden',
-    404 => 'Not found'
+    404 => 'Not Found',
+    405 => 'Method Not Allowed',
+    406 => 'Not Acceptable',
+    407 => 'Proxy Authentication Required',
+    408 => 'Request Timeout',
+    409 => 'Conflict',
+    410 => 'Gone',
+    411 => 'Length Required',
+    412 => 'Precondition Failed',
+    413 => 'Request Entity Too Large',
+    414 => 'Request-URI Too Long',
+    415 => 'Unsupported Media Type',
+    416 => 'Requested Range Not Satisfiable',
+    417 => 'Expectation Failed',
+    418 => 'I\'m a teapot',
+    429 => 'Too Many Requests',
+    
+    500 => 'Internal Server Error',
+    501 => 'Not Implemented',
+    502 => 'Bad Gateway',
+    503 => 'Service Unavailable',
+    504 => 'Gateway Timeout',
+    505 => 'HTTP Version Not Supported'
   }
+  
+  code = 418 if ! message[code]
   
   obj = {
     'error' => {
