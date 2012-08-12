95695c823fbda5ca3c210b686a7dda0359d031ed
Jacob Bijani <jacob.bijani@gmail.com>
2012-08-11 23:07:25 -0400

    2 tab to 4 tab

--- app.rb
+++ app.rb
@@ -4,27 +4,27 @@ require 'erb'
 #require 'json'
 
 before do
-    @pad = false
-    if ['json.dev', 'json.mobi'].include? request.host then
-        content_type 'application/json'
-    elsif ['jsonp.dev', 'jsonp.mobi'].include? request.host then
-        content_type 'application/javascript'
-        @pad = true
-    elsif ['edit.json.dev', 'edit.jsonp.dev', 'edit.json.mobi', 'edit.json.mobi'].include? request.host then
-        erb :edit_index, :layout => :edit
-    end
+  @pad = false
+  if ['json.dev', 'json.mobi'].include? request.host then
+    content_type 'application/json'
+  elsif ['jsonp.dev', 'jsonp.mobi'].include? request.host then
+    content_type 'application/javascript'
+    @pad = true
+  elsif ['edit.json.dev', 'edit.jsonp.dev', 'edit.json.mobi', 'edit.json.mobi'].include? request.host then
+    erb :edit_index, :layout => :edit
+  end
 end
 
 def pad(obj)
-    (params[:callback] ? params[:callback] : "json_mobi") + "(" + obj + ");"
+  (params[:callback] ? params[:callback] : "json_mobi") + "(" + obj + ");"
 end
 
 get '/:key' do
-    if @pad then pad(params[:key])
-    else params[:key] end
+  if @pad then pad(params[:key])
+  else params[:key] end
 end
 
 get '/' do
-    if @pad then pad("{json:'yup'}")
-    else "{json:'yup'}" end
+  if @pad then pad("{json:'yup'}")
+  else "{json:'yup'}" end
 end
